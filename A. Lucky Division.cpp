76272873a404c44ee6492303d8a3a5cc77173cbc
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-25 14:48:26 +0700

    Create A. Lucky Division.cpp

---
+++ A. Lucky Division.cpp
@@ -0,0 +1,61 @@
+// Problem: A. Lucky Division
+// Contest: Codeforces - Codeforces Beta Round #91 (Div. 2 Only)
+// URL: https://codeforces.com/problemset/problem/122/A
+// Memory Limit: 256 MB
+// Time Limit: 2000 ms
+// Who: kittisakp
+// Email: kittisakp.work@gmail.com
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a)                     \
+  for (auto x : a) cout << x << " "; \
+  cout << endl
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
+using namespace std;
+
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) {
+  cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+  const char* comma = strchr(names + 1, ',');
+  cout.write(names, comma - names) << " : " << arg1 << " | ";
+  __f(comma + 1, args...);
+}
+
+void solve() {
+  int n;
+  cin >> n;
+  int a[] = {4, 7, 47, 74, 44, 444, 447, 474, 477, 777, 774, 744};
+
+  for (int i = 0; i < 12; i++) {
+    if (n % a[i] == 0) {
+      cout << "YES";
+      return;
+    }
+  }
+  cout << "NO";
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  clock_t z = clock();
+
+  int t = 1;
+  // cin >> t;
+  while (t--) solve();
+
+  cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+  return 0;
+}
