9a885d119c58762b9e80b7814ea75e01344c8623
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-27 14:03:36 +0700

    Add template

---
+++ template_coderunner_c++.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <stack>
+#include <queue>
+#include <deque>
+
+#define int long long int
+#define double long double
+
+using namespace std;
+
+void solve() {
+	
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt", "r", stdin);
+//		freopen("output.txt", "w", stdout);
+	#endif
+	
+	int t = 1;
+	// cin >> t;
+	while (t--) solve();
+	
+	return 0;
+}
