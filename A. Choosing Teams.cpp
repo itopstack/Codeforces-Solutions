6db67a5decbbfa41323497cbaed1e88921689ca4
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-27 18:57:52 +0700

    Solve A. Choosing Teams

---
+++ A. Choosing Teams.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+
+using namespace std;
+
+const bool LOOP_TEST_CASES = false;
+
+void solve() {
+	int n, k, i;
+	cin >> n >> k;
+	int cnt = 0;
+
+	while (n--) {
+		cin >> i;
+		if (5 - i >= k) {
+			cnt++;
+		}
+	}
+
+	cout << cnt / 3;
+}
+
+int32_t main() {
+	ios_base::sync_with_stdio(0); 
+	cin.tie(0); 
+	cout.tie(0);
+
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+	
+	clock_t z = clock();
+
+	int t = 1;
+	if (LOOP_TEST_CASES) {
+		cin >> t;
+	}
+	while (t--) solve();
+
+	cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+	return 0;
+}
