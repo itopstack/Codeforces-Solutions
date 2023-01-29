cc3568f9e408be83e57d3ac220f68318cf71a7ef
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-29 19:08:05 +0700

    Solve Again Twenty Five

---
+++ A_Again_Twenty_Five.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+
+using namespace std;
+
+const bool LOOP_TEST_CASES = false;
+
+void solve() {
+    cout << 25;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(0); 
+    cin.tie(0); 
+    cout.tie(0);
+
+    int t = 1;
+    if (LOOP_TEST_CASES) {
+        cin >> t;
+    }
+    while (t--) solve();
+
+    return 0;
+}
