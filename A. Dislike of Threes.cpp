476c426cd3994cff9598ecf874a5a8e03ac79dd3
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-01-30 11:41:09 +0700

    Create A. Dislike of Threes.cpp

---
+++ A. Dislike of Threes.cpp
@@ -0,0 +1,60 @@
+// Problem: A. Dislike of Threes
+// Contest: Codeforces - Codeforces Round #739 (Div. 3)
+// URL: https://codeforces.com/problemset/problem/1560/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+//
+// Powered by CP Editor (https://cpeditor.org)
+
+#include <bits/stdc++.h>
+
+#define int long long int
+#define double long double
+#define print(a)                     \
+  for (auto x : a) cout << x << " "; \
+  cout << endl
+#define bug(...) __f(#__VA_ARGS__, __VA_ARGS__)
+
+using namespace std;
+
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1) {
+  cout << name << " : " << arg1 << endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args) {
+  const char* comma = strchr(names + 1, ',');
+  cout.write(names, comma - names) << " : " << arg1 << " | ";
+  __f(comma + 1, args...);
+}
+
+void solve() {
+  int k;
+  cin >> k;
+
+  for (int i = 1;; i++) {
+    if (i % 3 == 0 || i % 10 == 3) {
+      continue;
+    }
+    if (--k == 0) {
+      cout << i << endl;
+      break;
+    }
+  }
+}
+
+int32_t main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  clock_t z = clock();
+
+  int t = 1;
+  cin >> t;
+  while (t--) solve();
+
+  cerr << "Run Time : " << ((double)(clock() - z) / CLOCKS_PER_SEC) << endl;
+
+  return 0;
+}
