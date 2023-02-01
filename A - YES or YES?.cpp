d12b6ffd7103608dd6c5ec6d4eccf2f9ba174702
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2023-02-02 01:08:56 +0700

    Create A - YES or YES?.cpp

---
+++ A - YES or YES?.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+ 
+#define int long long int
+#define double long double
+#define print(a) for (auto x: a) cout << x << " "; cout << endl
+ 
+using namespace std;
+ 
+template <typename Arg1>
+void __f (const char* name, Arg1&& arg1) { cout << name << " : " << arg1 << endl; }
+template <typename Arg1, typename... Args>
+void __f (const char* names, Arg1&& arg1, Args&&... args)
+{
+	const char* comma = strchr (names + 1, ',');
+	cout.write (names, comma - names) << " : " << arg1 << " | "; __f (comma + 1, args...);
+}
+ 
+const bool LOOP_TEST_CASES = true;
+ 
+void solve() {
+	string s;
+	cin >> s;
+	transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+	if (s == "yes") {
+		cout << "YES" << endl;
+	} else {
+		cout << "NO" << endl;
+	}
+}
+ 
+int32_t main() {
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	#endif
+ 
+	int t = 1;
+	if (LOOP_TEST_CASES) {
+		cin >> t;
+	}
+	while (t--) solve();
+ 
+	return 0;
+}
